aa1020293269eee9af304099c414940cbfca1437
yejineeeeeeeee <124111019+Yegenie1@users.noreply.github.com>
2023-04-28 09:23:44 +0900

    springMongoDB2

---
+++ springMongoDB2/src/main/java/com/multi/mongoDB2/MemoDAO.java
@@ -0,0 +1,23 @@
+package com.multi.mongoDB2;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemoDAO {
+	@Autowired
+	MongoTemplate mongo;
+
+	public List<MemoVO> list() {
+		Query query = new Query();
+		query.with(new Sort(Sort.Direction.DESC, "date"));
+		List<MemoVO> list = mongo.find(query, MemoVO.class, "memo");
+		return list;
+
+	}
+}
